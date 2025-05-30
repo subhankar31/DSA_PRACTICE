ccca875262b2589bfc58d26b4b85355adf62eb61
RakeshSubhankar <subhankar313131@gmail.com>
2025-05-30 19:19:21 +0530

    Binary Search HARD Questions

--- src/binarysearch/BinarySearchPractice.java
+++ src/binarysearch/BinarySearchPractice.java
@@ -1,5 +1,9 @@
 package binarysearch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class BinarySearchPractice {
 
     /**
@@ -31,4 +35,46 @@ public class BinarySearchPractice {
         //didn't find anything
         return -1;
     }
+    /**
+     * BOOK ALLOCATION PROBLEM
+     * BINARY SEARCH HARD QUESTION BASED ON MIN MAX PATTERN
+     * Article url - https://takeuforward.org/data-structure/allocate-minimum-number-of-pages/
+     */
+        public static int countStudents(ArrayList<Integer> arr, int pages) {
+            int n = arr.size(); // size of array
+            int students = 1;
+            long pagesStudent = 0;
+            for (int i = 0; i < n; i++) {
+                if (pagesStudent + arr.get(i) <= pages) {
+                    // add pages to current student
+                    pagesStudent += arr.get(i);
+                } else {
+                    // add pages to next student
+                    students++;
+                    pagesStudent = arr.get(i);
+                }
+            }
+            return students;
+        }
+
+        public static int findPages() {
+            ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(25, 46, 28, 49, 24));
+            int n = arr.size();
+            int m = 4;
+            // book allocation impossible
+            if (m > n)
+                return -1;
+            int low = Collections.max(arr);
+            int high = arr.stream().mapToInt(Integer::intValue).sum();
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                int students = countStudents(arr, mid);
+                if (students > m) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            return low;
+        }
 }
