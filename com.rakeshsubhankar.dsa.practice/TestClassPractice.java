f5da0785812a824522f42c5c96cb765ebc405798
RakeshSubhankar <subhankar313131@gmail.com>
2026-03-01 10:21:56 +0530

    Revision

--- com.rakeshsubhankar.dsa.practice/TestClassPractice.java
+++ com.rakeshsubhankar.dsa.practice/TestClassPractice.java
@@ -157,5 +157,22 @@ public class TestClassPractice {
 
         }
     }
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        k=n-k;
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+        reverse(nums,0,n-1);
+    }
+    void reverse(int []nums, int i,int j){
+        while(i<=j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+    }
 
 }
--- com.rakeshsubhankar.dsa.practice/revision/AllPatternsRevisionReferenceNotes.java
+++ com.rakeshsubhankar.dsa.practice/revision/AllPatternsRevisionReferenceNotes.java
@@ -1,9 +1,8 @@
 package revision;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import dsapatternsseventyquestions.util.ListNode;
+
+import java.util.*;
 
 public class AllPatternsRevisionReferenceNotes {
     /**
@@ -188,6 +187,324 @@ public class AllPatternsRevisionReferenceNotes {
 
     }
     //Q-9
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer> cache = new HashMap<>();
+        cache.put(0,1);
+        int sum=0;
+        int total=0;
+        for(int i=0;i< nums.length;i++){
+            sum+=nums[i];
+            if (cache.containsKey(sum-k)){
+                total+=cache.get(sum-k);
+            }
+            cache.put(sum,cache.getOrDefault(sum,0)+1);
+        }
+        return total;
+    }
+
+    // q-10
+    public int trap(int[] height) {
+        int left=0;
+        int right= height.length-1;
+        int leftMax=height[left];
+        int rightMax=height[right];
+        int water=0;
+        while (left<right){
+            if (leftMax<rightMax){
+                left++;
+                leftMax=Math.max(leftMax,height[left]);
+                water+=leftMax-height[left];
+            }else{
+                right--;
+                rightMax=Math.max(rightMax,height[right]);
+                water+=rightMax-height[right];
+            }
+        }
+        return water;
+    }
+    //String and Hashing
+    //Q-11
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map=new HashMap<>();
+        for(String str:strs){
+            char [] charArray =str.toCharArray();
+            Arrays.sort(charArray);
+            String key=new String(charArray);
+            map.computeIfAbsent(key,k-> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+    //Q-12
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> resullt=new ArrayList<>();
+        //create 2 Arrays to store and compare
+        int [] pChar=new int[26];
+        int [] window=new int[26];
+        for (int i=0;i<p.length();i++){
+            pChar[p.charAt(i)-'a']++;
+            window[s.charAt(i)-'a']++;
+        }
+        if (Arrays.equals(window,pChar)){
+            resullt.add(0);
+        }
+        //slide the window
+        for(int i=p.length();i<s.length();i++){
+            window[s.charAt(i)-'a']++; //Added
+            //remove
+            window[s.charAt(i-p.length())]--;
+            if (Arrays.equals(window,pChar)){
+                resullt.add(i-p.length()+1); //right-left+1
+            }
+        }
+        return resullt;
+    }
+
+    //Q-13
+    public String longestPalindrome(String s) {
+        if (s.length()==0 || s==null) return ""; //base case
+        int start=0;
+        int end=0;
+
+        for(int i=0;i<s.length();i++){
+            //odd
+            int oddLen=calculateLength(s,i,i);
+            //even
+            int evenLen=calculateLength(s,i,i+1);
+            //check MAx
+            int len=Math.max(oddLen,evenLen);
+            //update the start and end
+            if (len>end-start){
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        //return the resultant string
+        return s.substring(start,end+1);
+
+    }
+    int calculateLength(String s,int start,int end){
+        while(start>=0 && end<s.length() && s.charAt(start)==s.charAt(end)){
+            start--;
+            end++;
+        }
+        return end-start-1;
+    }
+
+    //Q-14
+    int calculateTotalPanindrome(String s){
+        int total=0;
+        for(int center=0;center<s.length();center++){
+            total+=calculateLen(s,center,center);
+            total+=calculateLen(s,center,center+1);
+        }
+        return total;
+    }
+    int calculateLen(String s, int start , int end){
+        int total=0;
+        while (start>=0 && end<s.length() && s.charAt(start)==s.charAt(end)){
+            total++;
+            start--;
+            end++;
+        }
+        return total;
+    }
+    //Q-15 Binary Search
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        while (start<=end){
+            int mid=start+end/2;
+            if (nums[mid]==target) return mid;
+            //if left is sorted
+            if (nums[start]<=nums[mid]){
+                if (nums[start]<=target && target <=nums[end]){
+                    end=mid-1;
+                }else start=mid+1;
+            }else{
+                if(nums[mid]<=target && target <= nums[end]){
+                    start=mid+1;
+                }else end=mid-1;
+            }
+        }
+        return -1;
+    }
+
+    //Q-16
+    public int[] searchRange(int[] nums, int target) {
+        int first=findFirst(nums,target);
+        int last=findLast(nums,target);
+        return new int[]{first,last};
+    }
+    public int findFirst(int [] nums, int target ){
+        int result=-1;
+        int low=0;
+        int high=nums.length-1;
+        while (low<=high){
+            int mid=low+high/2;
+            if (nums[high]==target){
+                result=mid;
+                high=mid-1;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else low=mid+1;
+        }
+        return result;
+    }
+    public int findLast(int [] nums, int target ){
+        int result=-1;
+        int low=0;
+        int high=nums.length-1;
+        while (low<=high){
+            int mid=low+high/2;
+            if (nums[high]==target){
+                result=mid;
+                low=mid+1;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else low=mid+1;
+        }
+        return result;
+    }
+
+    //Q-17 Koko eating bananas
+    public int minEatingSpeed(int[] v, int h) {
+        int low=1;
+        int high=findHigh(v);
+        while (low<=high){
+            int mid=low+high/2;
+            if (possible(v,mid,h)){
+                //check for more low possible by going more left
+                high=mid-1;
+            }else low=mid+1;
+        }
+        return low;
+    }
+    boolean possible(int [] v, int hourly, int limit){
+        int total=0;
+        for(int i=0;i<v.length;i++){
+          total+=Math.ceil((double)(v[i])/(double)(hourly));
+        }
+        return (total<limit)? true:false;
+    }
+    int findHigh(int [] v){
+        int high=Integer.MIN_VALUE;
+        for(int i=0;i<v.length;i++){
+            high= Math.max(high,v[i]);
+        }
+        return high;
+    }
+    //q-18
+    public int minDays(int[] arr, int m, int k) {
+        //base case
+        if (m*k>arr.length) return -1;
+        int mini=Integer.MAX_VALUE;
+        int maxi=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            mini= Math.min(mini,arr[i]);
+            maxi=Math.max(maxi,arr[i]);
+        }
+        int low=mini;
+        int high=maxi;
+        while (low<=high){
+            int mid=low+high/2;
+            if (possibleBq(mid,m,k,arr)){
+                high=mid-1;
+            }else low=mid+1;
+        }
+        return low;
+    }
+    boolean possibleBq(int days,int m , int k , int [] arr){
+        int count=0;
+        int noBq=0;
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]<=days){
+                count++;
+            }else{
+                noBq+=count/k;
+                count=0;
+            }
+        }
+        //last one
+        noBq+=count/k;
+        return noBq>=m;
+    }
+    //Q-19
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int res=0;
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        int prev=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            if (prev>intervals[i][1]){
+                res++;
+            }else{
+                //update the prev
+                prev=intervals[i][1];
+            }
+        }
+        return res;
+    }
+    //Q-20
+    public String reorganizeString(String s) {
+     return null;
+    }
+
+    //21
+    public int[] topKFrequent(int[] nums, int k) {
+        //freq map to store int and its freq
+        Map<Integer,Integer> freqMap=new HashMap<>();
+        for (int num:nums){
+            freqMap.put(num, freqMap.getOrDefault(num,0)+1);
+        }
+        //PQ to make min heap
+        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
+        //put to pq
+        for (Map.Entry<Integer,Integer> entry: freqMap.entrySet()){
+            pq.offer(entry);
+            if (pq.size()>k){
+                pq.poll();
+            }
+        }
+        int [] res=new int[k];
+        int counter=0;
+        while (!pq.isEmpty()){
+            res[counter++]=pq.poll().getKey();
+        }
+        return res;
+    }
+    //Q-22
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode dummy=new ListNode(-1);
+        ListNode curr=dummy;
+        //Pq to sort in Asc order
+        PriorityQueue<ListNode> pq= new PriorityQueue<>((a,b)->a.val-b.val); //Min heap
+        for(ListNode list:lists){
+            pq.offer(list);
+        }
+        while (!pq.isEmpty()){
+            ListNode node=pq.poll();
+            curr.next=node;
+            curr= curr.next;
+            if (node.next!=null){
+                pq.offer(node.next);
+            }
+        }
+
+
+        return dummy.next;
+    }
+    //Q-23
+    public int climbStairs(int n) {
+        if (n==0 || n==1) return 1;
+        int [] memo=new int[n+1];
+        return climb(n,memo);
+    }
+    int climb(int n, int [] memo){
+        if (n==0 || n==1) return 1;
+        if (memo[n]>0) return memo[n];
+        memo[n]=climb(n-1,memo) + climb(n-2,memo);
+        return memo[n];
+    }
+
     public static void main(String[] args) {
 
     }
